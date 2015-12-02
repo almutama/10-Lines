5c46245cdd7577ca6821bea031d5a0dab17506b6
nyashacsmith <nyashacsmith@gmail.com>
2015-12-01 17:22:12 -0800

    Created a invite screen

--- TenLines/InvitesViewController.swift
+++ TenLines/InvitesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class InvitesViewController: UITableViewController {
-
+    private var invites: Array<Artist>?
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +20,9 @@ class InvitesViewController: UITableViewController {
         self.view.backgroundColor = UIColor(white: 0.96, alpha: 1.0)
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
 
+        // Load friends right away.
+        self.refreshInvites(nil)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,6 +31,12 @@ class InvitesViewController: UITableViewController {
     }
     
     func refreshInvites(sender: AnyObject?) {
+        // Temporary load feed data from a file. Eventually we want to get this
+        // data by invoking a web service instead.
+        let path = NSBundle.mainBundle().pathForResource("invites", ofType: "json")
+        let data = JSON(data: NSData(contentsOfFile: path!)!)
+        invites = Artist.fromJSON(data)
+        
         // Reload data.
         self.tableView.reloadData()
         
@@ -39,23 +48,52 @@ class InvitesViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return invites!.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
+        
         // Configure the cell...
-
+        let iconImageView: UIImageView = cell.viewWithTag(100) as! UIImageView
+        cell.backgroundColor = UIColor.clearColor()
+        iconImageView.clipsToBounds = true
+        iconImageView.layer.cornerRadius = 50
+        iconImageView.layer.borderWidth = 4
+        iconImageView.layer.borderColor = UIColor(red: 0.6, green: 0.93, blue: 0.85, alpha: 1.0).CGColor
+        
+        // Get artist.
+        let artist = invites![indexPath.row]
+        
+        // Name label.
+        let nameLabel: UILabel = cell.viewWithTag(11) as! UILabel
+        nameLabel.text = artist.firstname + " invited you!"
+        
+        // Profile picture.
+        if (artist.icon != nil) {
+            iconImageView.image = artist.icon
+        }
+        else {
+            { artist.loadIcon() } ~> { iconImageView.image = artist.icon }
+        }
+        
+        // Set acessory view based on selection state.
+        let selectedRows = tableView.indexPathsForSelectedRows
+        if (selectedRows != nil && selectedRows!.contains(indexPath)) {
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        }
+        else {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
